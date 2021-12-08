82c80633b245dc409736d7163a1f2dba66c965ce
Andrea Di Pierno <a.dipierno99@gmail.com>
2021-12-08 23:27:17 +0100

    Correzione logica scoping

--- src/it/unisa/tables/TypeEnvironment.java
+++ src/it/unisa/tables/TypeEnvironment.java
@@ -8,52 +8,9 @@ public class TypeEnvironment {
     int currentIndex;
 
     public TypeEnvironment() {
-        index = 0; //indice alla tabella corrente
+        index = 0;
         currentIndex = -1;
         this.typeEnvironment = new ArrayList<Table>();
-    }
-
-    public ArrayList<Table> getTypeEnvironment() {
-        return typeEnvironment;
-    }
-
-    public void setTypeEnvironment(ArrayList<Table> typeEnvironment) {
-        this.typeEnvironment = typeEnvironment;
-    }
-
-    // restituisce null se non trova l'elemento
-    public Row lookup(String symbol) {
-        return typeEnvironment.get(index).find(symbol);//prende la tabella corrente e fa ricerca
-    }
-
-    //Avviare un nuovo ambito
-    public void enterScope() {
-        Table newTable;
-        if (typeEnvironment.size() > 0) //abbiamo tabelle già inserite
-            newTable = new Table(typeEnvironment.get(index), index); //crea una nuova tabella usando il riferimento a quella del padre
-        else // altrimenti crea da zero
-            newTable = new Table();
-        index = typeEnvironment.size();
-        typeEnvironment.add(newTable);
-    }
-
-    public void enterScopeClang() {
-        currentIndex++;
-        index = currentIndex;
-    }
-
-    public void exitScope() {
-        index = typeEnvironment.get(index).parentIndex;
-    }
-
-    public void addId(String id, Row row) {
-        //se già presente
-        if (probe(id)) {
-            System.err.println("Multiple declaration for " + id);
-            System.exit(1);
-        }
-        Table table = typeEnvironment.get(index);
-        table.addRow(id, row);
     }
 
     public boolean probe(String id) {
@@ -66,5 +23,45 @@ public class TypeEnvironment {
 
     public void setIndex(int index) {
         this.index = index;
+    }
+
+    public ArrayList<Table> getTypeEnvironment() {
+        return typeEnvironment;
+    }
+
+    public void setTypeEnvironment(ArrayList<Table> typeEnvironment) {
+        this.typeEnvironment = typeEnvironment;
+    }
+
+    public Row lookup(String symbol) {
+        return typeEnvironment.get(index).find(symbol);
+    }
+
+    public void scopeIn() {
+        Table newTable;
+        if (typeEnvironment.size() > 0)
+            newTable = new Table(typeEnvironment.get(index), index);
+        else
+            newTable = new Table();
+        index = typeEnvironment.size();
+        typeEnvironment.add(newTable);
+    }
+
+    public void scopeInClang() {
+        currentIndex++;
+        index = currentIndex;
+    }
+
+    public void scopeOut() {
+        index = typeEnvironment.get(index).parentIndex;
+    }
+
+    public void addId(String id, Row row) {
+        if (probe(id)) {
+            System.err.println("Multiple declaration for " + id);
+            System.exit(1);
+        }
+        Table table = typeEnvironment.get(index);
+        table.addRow(id, row);
     }
 }
